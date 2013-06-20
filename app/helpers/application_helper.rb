629b87118b99ed937334c82126cc495f6049439e
Matt McKenna <matt@omadahealth.com>
2013-06-20 15:45:27 -0700

    Making the default app_envs more sane

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def javascript_wattle
-     "<script>watCatcher.appEnvsToWorryAbout= [\"demo\", \"staging\", \"production\", \"development\", \"goobilygoo\"];watCatcher.appEnv= \"#{Rails.env}\"</script>".html_safe
+     "<script>watCatcher.appEnvsToWorryAbout= [\"demo\", \"staging\", \"production\"];watCatcher.appEnv= \"#{Rails.env}\"</script>".html_safe
   end
 end
