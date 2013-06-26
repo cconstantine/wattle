81b151648e1d0c976f2b1916e33757dc53278612
Chris Constantine <chris@omadahealth.com>
2013-06-25 17:10:50 -0700

    Upgrade wat_catcher

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def javascript_wattle
-     "<script>watCatcher.appEnvsToWorryAbout= [\"demo\", \"staging\", \"production\", \"development\"];watCatcher.appEnv= \"#{Rails.env}\";</script>".html_safe
+     "<script>watCatcher.appEnv= \"#{Rails.env}\";</script>".html_safe
   end
 
   def checked? param, key
