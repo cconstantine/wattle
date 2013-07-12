c8b4d79d50e4affc5ab19ad984f4ff04c5063cc4
Chris Constantine <pair+cconstantine@omadahealth.com>
2013-07-12 13:57:57 -0700

    Minor cleanup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,10 +6,7 @@ module ApplicationHelper
   end
 
   def wats(grouping)
-    Rails.logger.debug "===== Helper grouping wats: #{grouping.wats.map(&:app_env)}"
-
     filtered = grouping.wats.filtered(filters)
-    Rails.logger.debug "===== Helper wats: #{filtered.all}"
     filtered
   end
 
