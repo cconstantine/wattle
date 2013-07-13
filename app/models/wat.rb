95cf2de45460b919912da2b10846615467fbe48f
Austin Putman <dev+austinfromboston@omadahealth.com>
2013-07-13 16:56:55 -0700

    [#53272017] improve display of request headers for Wats

--- app/models/wat.rb
+++ app/models/wat.rb
@@ -8,8 +8,7 @@ class Wat < ActiveRecord::Base
   after_create :construct_groupings!, :send_emails
 
   scope :filtered, ->(opts={}) {
-    Rails.logger.debug "===== opts: #{opts}"
-    running_scope = Wat.all
+    running_scope = all
     running_scope = running_scope.joins(:groupings).where("groupings.state" => opts[:state]) if opts[:state]
     running_scope = running_scope.where(:app_name => opts[:app_name]) if opts[:app_name]
     running_scope = running_scope.where(:app_env  => opts[:app_env])  if opts[:app_env]
