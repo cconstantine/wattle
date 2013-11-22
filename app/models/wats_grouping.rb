481cfcd27dfd181d79f5c87b43bd9e5218d3c30a
Chris Constantine & Sam Pierson <pair+cconstantine+sampierson@omadahealth.com>
2013-11-22 10:28:24 -0800

    Derp, actually get rid of it

--- app/models/wats_grouping.rb
+++ app/models/wats_grouping.rb
@@ -1,6 +1,6 @@
 class WatsGrouping < ActiveRecord::Base
   belongs_to :wat
-  belongs_to :grouping, counter_cache: :wats_count
+  belongs_to :grouping
 
   validates :wat, presence: true
   validates :grouping, presence: true
