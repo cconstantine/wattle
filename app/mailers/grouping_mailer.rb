423226f076650832fee584bf6638b4514a14efad
lilliealbert <lillie@omadahealth.com>
2013-11-22 18:15:31 -0800

    Replacing the message in the subject with a SHA1. Removing message from subject.

--- app/mailers/grouping_mailer.rb
+++ app/mailers/grouping_mailer.rb
@@ -1,3 +1,5 @@
+require 'digest/sha1'
+
 class GroupingMailer < ActionMailer::Base
   default :from => "wattle@example.com"
   layout "mailer"
@@ -8,6 +10,6 @@ class GroupingMailer < ActionMailer::Base
     @new_count = grouping.last_emailed_at.present? ? grouping.wats.where('wats.created_at > ?', grouping.last_emailed_at).count : grouping.wats.count
 
     @app_envs = @grouping.wats.pluck(:app_env).uniq
-    mail :to => Watcher.pluck(:email), :subject => "[#{@wat.app_name} #{@app_envs.map {|x| "##{x[0..3]}"}.join(" ")}] #{@wat.message}"
+    mail :to => Watcher.pluck(:email), :subject => "[#{@wat.app_name} #{@app_envs.map {|x| "##{x[0..3]}"}.join(" ")}] #{Digest::SHA1.hexdigest(@wat.message)}"
   end
 end
