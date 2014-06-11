46a57ca21227c4406f44341ea9a5e89c25233427
Chris Constantine <chris@omadahealth.com>
2014-06-11 13:11:13 -0700

    Allow developer auth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :developer unless Rails.env.production?
   if ENV['GOOGLE_KEY'].present? || Secret.respond_to?(:google_key)
     provider :gplus, ENV['GOOGLE_KEY'] || Secret.google_key, ENV['GOOGLE_SECRET'] || Secret.google_secret, scope: 'userinfo.email, userinfo.profile'
+  else
+    provider :developer
   end
 end
 OmniAuth.config.logger = Rails.logger
