604aa5fc8233187fed417219950130fd93e9a88c
Chris Constantine <chris@omadahealth.com>
2014-12-09 16:31:46 -0800

    Daemonize puma and sidekiq

--- config/puma.rb
+++ config/puma.rb
@@ -6,3 +6,11 @@ preload_app!
 rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
+
+
+if Rails.env.production?
+  daemonize true
+  pidfile Rails.root.join('tmp','pids', 'puma.pid')
+  stdout_redirect Rails.root.join('log', 'puma.stdout.log'), Rails.root.join('log', 'puma.stderr.log'), true
+end
+
