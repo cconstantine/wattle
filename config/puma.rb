82443c9da9fc5c3015289f8aad599a7c64cb6e9b
Chris Constantine <chris@omadahealth.com>
2014-12-10 11:37:06 -0800

    More correct puma.rb for listening to sockets

--- config/puma.rb
+++ config/puma.rb
@@ -6,3 +6,9 @@ preload_app!
 rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
+
+rails_root = File.expand_path(File.join(File.dirname(__FILE__), ".."))
+bind "unix://#{ File.join(rails_root,'sockets','wattle.sock') }"
+daemonize true
+pidfile File.join(rails_root, 'tmp','pids', 'puma.pid')
+stdout_redirect File.join(rails_root, 'log', 'puma.stdout.log'), File.join(rails_root, 'log', 'puma.stderr.log'), true
