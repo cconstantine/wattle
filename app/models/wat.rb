a4162b910cc4f9ba98c66b193900e3c35042a533
Chris Constantine <chris@omadahealth.com>
2015-01-05 11:41:26 -0800

    Stop using broken redis semaphore

--- app/models/wat.rb
+++ app/models/wat.rb
@@ -117,11 +117,7 @@ SQL
   end
 
   def construct_groupings!
-    Sidekiq::redis do |redis|
-      Redis::Semaphore.new(:"Wat#construct_groupings!", :connection => redis).lock(1.hour) do
-        self.groupings = (Grouping.matching(self) << Grouping.get_or_create_from_wat!(self)).uniq
-      end
-    end
+    self.groupings = (Grouping.matching(self) << Grouping.get_or_create_from_wat!(self)).uniq
   end
 
   def send_email
