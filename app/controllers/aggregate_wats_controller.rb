2cca7af0cf6ba2f8127d1e43f1b2070b61813720
Austin Putman <austin@rawfingertips.com>
2015-04-02 07:46:25 -0400

    [#91624664] fix ordering for aggregate wats controller

--- app/controllers/aggregate_wats_controller.rb
+++ app/controllers/aggregate_wats_controller.rb
@@ -12,6 +12,6 @@ class AggregateWatsController < ApplicationController
   def count_by_timescale(timescale)
     date_segment = Wat.send(:sanitize_conditions,["date_trunc(?, created_at)",timescale])
     grouped_query = Wat.group(date_segment)
-    grouped_query.order(1).count #order by the first column, since we cant predict the aggregate alias assigned by ActiveRecord
+    grouped_query.order('2 desc').count #order by the time column(2nd), since we cant predict the aggregate alias assigned by ActiveRecord
   end
 end
