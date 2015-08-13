a540924e6b51587fd228cee263e7f1d6d17548fc
Chris Constantine <chris@omadahealth.com>
2015-08-13 12:46:19 -0700

    Update to new wat_catcher and make the failed_worker even better (worse?)

--- app/workers/failed_worker.rb
+++ app/workers/failed_worker.rb
@@ -1,10 +1,15 @@
 class FailedWorker
   include Sidekiq::Worker
 
+  sidekiq_options :retry => false
+
   def perform(an_argument)
     foo(an_argument)
   end
 
+  def wat_user(too, many, args)
+    {id: "wat?"}
+  end
 
   private
   def foo(an_argument)
