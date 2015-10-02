a50bae87e03870e9c44add3b81be90eb24678f79
Chris Constantine <chris@omadahealth.com>
2015-10-02 14:48:01 -0700

    Use web.sock

--- config/puma.rb
+++ config/puma.rb
@@ -11,6 +11,6 @@ port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
 rails_root = File.expand_path(File.join(File.dirname(__FILE__), ".."))
-bind "unix://#{ File.join(rails_root,'sockets','wattle.sock') }"
+bind "unix://#{ File.join(rails_root,'sockets','web.sock') }"
 
 stdout_redirect File.join(rails_root, 'log', 'puma.stdout.log'), File.join(rails_root, 'log', 'puma.stderr.log'), true
