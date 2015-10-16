97e4728e1d01c85225e0e4e3678feaf74e816549
Chris Constantine <chris@omadahealth.com>
2015-10-16 11:17:32 -0700

    Move up a dir

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -5,7 +5,7 @@ preload_app!
 
 rackup      DefaultRackup
 
-rails_root = File.expand_path(File.join(File.dirname(__FILE__), ".."))
+rails_root = File.expand_path(File.join(File.dirname(__FILE__), "..", ".."))
 bind "unix://#{ File.join(rails_root,'sockets','app.sock') }"
 
 stdout_redirect File.join(rails_root, 'log', 'puma.stdout.log'), File.join(rails_root, 'log', 'puma.stderr.log'), true
