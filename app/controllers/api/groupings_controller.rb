e92c5a5da6f985d95f202ec93ba546d3dfe45f9d
Vincent Coste <coste.vincent@gmail.com>
2015-11-12 11:19:39 -0800

    added more filters to API

--- app/controllers/api/groupings_controller.rb
+++ app/controllers/api/groupings_controller.rb
@@ -3,8 +3,8 @@ module Api
 	 respond_to :json
 
 		def count_by_state
-			if params[:app_name]
-				@groupings = Grouping.where("groupings.updated_at > ?", 7.days.ago).app_name(params[:app_name]).group(:state).count
+			if params[:app_name] && params[:app_env] && params[:language]
+				@groupings = Grouping.where("groupings.updated_at > ?", 7.days.ago).language(params[:language]).app_name(params[:app_name]).app_env(params[:app_env]).group(:state).count
 
 				respond_to do |format|
 	      	format.json  { render :json => @groupings }
--- spec/controllers/api/groupings_controller_spec.rb
+++ spec/controllers/api/groupings_controller_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Api::GroupingsController, type: :controller do
   let(:watcher) { watchers(:default) }
+  let(:app_name) { "app1" }
 
   before do
     login watcher
@@ -9,19 +10,28 @@ describe Api::GroupingsController, type: :controller do
 
   describe "#count_by_state" do 
     context "with correct params" do 
-      let(:app_name) { "app1" }
-      subject { get :count_by_state, format: :json, app_name:app_name }
+      subject { get :count_by_state, format: :json, app_name:app_name, app_env:'production', language:'ruby' }
 
       context "when app with wats" do
         it "return the right count of every groupings state" do
           subject
-          scoped_grouping = Grouping.app_name(app_name)
+          scoped_grouping = Grouping.app_name(app_name).app_env('production').language('ruby')
           expect(response.body).to be_json_eql({ unacknowledged: scoped_grouping.unacknowledged.count, acknowledged: scoped_grouping.state(:acknowledged).count, deprioritized: scoped_grouping.deprioritized.count, resolved: scoped_grouping.resolved.count }.to_json)
         end
       end
     end
     context 'without :app_name params' do
-      subject { get :count_by_state, format: :json }
+      subject { get :count_by_state, app_env:'test', language:'ruby',  format: :json }
+
+      it { is_expected.not_to be_success }
+    end
+    context 'without :app_env params' do
+      subject { get :count_by_state, app_name:app_name, language:'ruby',  format: :json }
+
+      it { is_expected.not_to be_success }
+    end
+    context 'without :language params' do
+      subject { get :count_by_state, app_name:app_name, app_env:'test', format: :json }
 
       it { is_expected.not_to be_success }
     end
