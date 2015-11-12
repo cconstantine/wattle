514183b56cb3fd8773924c8036e30c3df35bd8dd
Vincent Coste <coste.vincent@gmail.com>
2015-11-11 17:16:40 -0800

    added time limit to retrieve groupings stats

--- app/controllers/api/groupings_controller.rb
+++ app/controllers/api/groupings_controller.rb
@@ -4,7 +4,7 @@ module Api
 
 		def count_by_state
 			if params[:app_name]
-				@groupings = Grouping.app_name(params[:app_name]).group(:state).count
+				@groupings = Grouping.where("groupings.updated_at > ?", 7.days.ago).app_name(params[:app_name]).group(:state).count
 
 				respond_to do |format|
 	      	format.json  { render :json => @groupings }
