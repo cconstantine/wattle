5930d2597ad842cefbacf5f46419e44b622df6ca
Vincent Coste <coste.vincent@gmail.com>
2015-12-21 09:40:01 -0800

    converted tab indent to spaces

--- app/controllers/api/groupings_controller.rb
+++ app/controllers/api/groupings_controller.rb
@@ -1,18 +1,18 @@
 module Api
-	class GroupingsController < ApplicationController
-	 respond_to :json
+  class GroupingsController < ApplicationController
+   respond_to :json
 
-		def count_by_state
-			if params[:app_name] && params[:app_env] && params[:language]
-				@groupings = Grouping.where("groupings.updated_at > ?", 7.days.ago).language(params[:language]).app_name(params[:app_name]).app_env(params[:app_env]).group(:state).count
+    def count_by_state
+      if params[:app_name] && params[:app_env] && params[:language]
+        @groupings = Grouping.where("groupings.updated_at > ?", 7.days.ago).language(params[:language]).app_name(params[:app_name]).app_env(params[:app_env]).group(:state).count
 
-				respond_to do |format|
-	      	format.json  { render :json => @groupings }
-	    	end
-	    else
-	    	render text: :ko, status: 400 
-	    end
-		end
+        respond_to do |format|
+          format.json  { render :json => @groupings }
+        end
+      else
+        render text: :ko, status: 400 
+      end
+    end
 
-	end
+  end
 end
