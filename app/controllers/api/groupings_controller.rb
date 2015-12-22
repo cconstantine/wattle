6b0c4a1cc9579a4a2a93625e2e2ef9a61afbf8ea
cconstantine <chris@omadahealth.com>
2015-12-21 16:28:26 -0800

    Improve performance of count_by_state query

--- app/controllers/api/groupings_controller.rb
+++ app/controllers/api/groupings_controller.rb
@@ -2,9 +2,9 @@ module Api
 	class GroupingsController < ApplicationController
 	 respond_to :json
 
-		def count_by_state
-			if params[:app_name] && params[:app_env] && params[:language]
-				@groupings = Grouping.where("groupings.updated_at > ?", 7.days.ago).language(params[:language]).app_name(params[:app_name]).app_env(params[:app_env]).group(:state).count
+    def count_by_state
+      if params[:app_name] && params[:app_env] && params[:language]
+        @groupings = Grouping.filtered(params).where("groupings.updated_at > ?", 7.days.ago).group(:state).count
 
 				respond_to do |format|
 	      	format.json  { render :json => @groupings }
