f2a33dbe97943168d6e5c43b135231caa3ca80a6
Myles Guanzon <mpg916@users.noreply.github.com>
2016-07-13 17:48:50 -0700

    [#126195263] Add default values for the daily_grouping_updater to accommodate the inability to use named parameters in sidekiq-cron (#10)

--- app/workers/daily_grouping_updater.rb
+++ app/workers/daily_grouping_updater.rb
@@ -5,7 +5,7 @@ class DailyGroupingUpdater
     @system_account = Watcher.retrieve_system_account
   end
 
-  def perform(app_name:, time_frame: 15.days.ago)
+  def perform(app_name: WatConfig.secret_value('SYSTEM_ACCOUNT_APPS'), time_frame: 15.days.ago)
     groupings = Grouping.app_name(app_name).retrieve_stale_groupings(time_frame)
     groupings.each do |grouping|
       grouping.whodunnit(@system_account) do
