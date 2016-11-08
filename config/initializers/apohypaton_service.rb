2ee4915e4e1dbcbff84b0f757d4a1b32a85922de
Kevin G. Phillips <kevin.phillips@omadahealth.com>
2016-11-08 14:04:40 -0800

    Remove Apohypaton services initializer

--- config/initializers/apohypaton_service.rb
+++
@@ -1,8 +0,0 @@
-service_name = ENV['APOHYPATON_SERVICE_NAME'] || ENV['UPSTART_JOB']
-
-if service_name
-  services = Apohypaton::Service.load_from_yaml_file(File.expand_path('../../apohypaton.yml', __FILE__))
-
-  matching_service = services.select { |s| s.name == service_name }.first
-  matching_service.save!
-end
