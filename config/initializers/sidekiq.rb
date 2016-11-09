0d33ef41c893c8a28d05d0a145e0ae2729544e31
cconstantine <chris@omadahealth.com>
2016-11-09 13:20:30 -0800

    Only load sidekiq schedule yaml file if there is a SYSTEM_ACCOUNT_APPS

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,5 @@
 schedule_file = "#{Rails.root}/config/sidekiq_schedule.yml"
 
-if File.exists?(schedule_file) && Sidekiq.server?
+if WatConfig.secret_value('SYSTEM_ACCOUNT_APPS').present? && File.exists?(schedule_file) && Sidekiq.server?
   Sidekiq::Cron::Job.load_from_hash YAML.load(ERB.new(File.read(schedule_file)).result)
 end
